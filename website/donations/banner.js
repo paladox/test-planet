a6ead053bd3e0e92cfa42fdb1110f777922d3f70
Víctor Blázquez <victor.blazquez@kde.org>
2015-08-09 14:50:35 +0000

    fix donation, https must be called instead of http

--- website/donations/banner.js
+++ website/donations/banner.js
@@ -1,5 +1,5 @@
 $(function() {
-    $.getJSON('http://planetkde.org/donations/get.php', function(data) {
+    $.getJSON('https://planetkde.org/donations/get.php', function(data) {
         $('#donation-goal-amt').html(data.goal);
         $('#donation-bar').css({
             'width': data.pcnt + '%',
