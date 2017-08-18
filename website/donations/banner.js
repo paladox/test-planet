5040b888d29ef9a51bd5adc0abd65133e067a798
Albert Astals Cid <aacid@kde.org>
2017-08-18 19:48:27 +0200

    Add fundraiser banner

--- website/donations/banner.js
+++ website/donations/banner.js
@@ -1,9 +1,9 @@
 $(function() {
     $.getJSON('https://planetkde.org/donations/get.php', function(data) {
         $('#donation-current-amount').html(data.current);
-//         $('#donation-bar').css({
-//             'width': data.pcnt + '%',
-//             'background': 'rgb(68,132,242)'
-//         });
+        $('#donation-bar').css({
+            'width': data.pcnt + '%',
+            'background': 'rgb(68,132,242)'
+        });
     });
 });
