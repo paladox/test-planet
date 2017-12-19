1b6cb6fc4a7b56447a60660b5694ea2ad2a5e59d
Albert Astals Cid <aacid@kde.org>
2017-12-19 01:17:31 +0100

    Enable the fundaiser banner for endofyear2017

--- website/donations/get.php
+++ website/donations/get.php
@@ -13,18 +13,19 @@ header('Access-Control-Allow-Origin: *');
 
 // Get the campaign data
 $dom = new simple_html_dom();
-$dom->load_file("https://www.kde.org/fundraisers/randameetings2017/");
+$dom->load_file("https://www.kde.org/fundraisers/yearend2017/");
 
 // For fundraisers with percentage
-$content = $dom->find('.progress-bar', 0)->style;
-$chunks = explode(' ', $content);
-$percent = $chunks[1];
-$percent = substr($percent, 0, -2);
+$content = $dom->find('.progress-value', 0)->plaintext;
+$content = trim($content);
+$percent = substr($content, 0, strpos($content, "%"));
 $percent = floatval($percent);
 
 // For fundraisers with current amount
-$content = $dom->find('.current-amount', 0)->plaintext;
-$current = substr($content, 6);
+$content = $dom->find('.text__blue', 0)->plaintext;
+$content = substr($content, strpos($content, "Raised:"));
+$content = substr($content, 8);
+$current = substr($content, 0, strpos($content, " "));
 
 // Output the data in JSON format
 $data = array(
